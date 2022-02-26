1e0c6e361c2e19030848059c37f30e94522a8f03
MomoLangenstein <MomoLangenstein@users.noreply.github.com>
2022-02-26 11:54:01 +0000

    deploy: 16ff5948c6f473213aff5205bf93079b95acc45e

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["const_type_layout"] = {"name":"","dirs":[{"name":"impls","dirs":[{"name":"alloc","files":["boxed.rs","mod.rs"]},{"name":"core","dirs":[{"name":"sync","files":["atomic.rs","mod.rs"]}],"files":["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","marker.rs","mem.rs","mod.rs","num.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs"]}],"files":["mod.rs"]}],"files":["lib.rs","ser.rs"]};
+sourcesIndex["const_type_layout"] = {"name":"","dirs":[{"name":"impls","dirs":[{"name":"alloc","files":["boxed.rs","mod.rs"]},{"name":"core","dirs":[{"name":"sync","files":["atomic.rs","mod.rs"]}],"files":["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","marker.rs","mem.rs","mod.rs","num.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs"]}],"files":["mod.rs"]}],"files":["lib.rs","ser.rs","serde.rs"]};
 createSourceSidebar();
