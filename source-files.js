b26d195e6a054a02412a215a1b99ae29a4764ddd
juntyr <juntyr@users.noreply.github.com>
2023-05-13 17:17:52 +0000

    deploy: 645de0e2116cfb9fa263e10b1e3c8500451f5d0a

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"const_type_layout":["",[["impls",[["alloc",[],["boxed.rs","mod.rs"]],["core",[["sync",[],["atomic.rs","mod.rs"]]],["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","fn.rs","marker.rs","mem.rs","mod.rs","num.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs","tuple.rs"]]],["mod.rs"]]],["lib.rs","ser.rs","serde.rs"]]\
+"const_type_layout":["",[["impls",[["core",[["sync",[],["atomic.rs","mod.rs"]]],["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","fn.rs","marker.rs","mem.rs","mod.rs","num.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs","tuple.rs"]]],["mod.rs"]]],["lib.rs","ser.rs","serde.rs"]]\
 }');
 createSourceSidebar();
