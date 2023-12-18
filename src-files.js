c8252a9e86440d3a39796fbf18007d2b56280ff0
juntyr <juntyr@users.noreply.github.com>
2023-12-18 14:21:13 +0000

    deploy: 7c0cb012a4e59c7bd9b6ce6f85e27b7d1c3cda0a

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"const_type_layout":["",[["impls",[["core",[["sync",[],["atomic.rs","mod.rs"]]],["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","fn.rs","marker.rs","mem.rs","mod.rs","num.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs","tuple.rs"]]],["mod.rs"]]],["inhabited.rs","lib.rs","ser.rs","typeset.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["const_type_layout",["",[["impls",[["core",[["sync",[],["atomic.rs","mod.rs"]]],["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","fn.rs","marker.rs","mem.rs","mod.rs","num.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs","tuple.rs"]]],["mod.rs"]]],["inhabited.rs","lib.rs","ser.rs","typeset.rs"]]]\
+]'));
 createSrcSidebar();
