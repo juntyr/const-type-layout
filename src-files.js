6db99d39792092a620c7940e736d40bf5b60a686
juntyr <juntyr@users.noreply.github.com>
2024-11-14 13:57:12 +0000

    deploy: 49bf2c0fe208a8ff0bc170e32b30ee72a0e7c634

--- src-files.js
+++ src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["const_type_layout",["",[["impls",[["core",[["sync",[],["atomic.rs","mod.rs"]]],["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","fn.rs","marker.rs","mem.rs","mod.rs","num.rs","ops.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs","tuple.rs"]]],["mod.rs"]]],["inhabited.rs","lib.rs","ser.rs","typeset.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["const_type_layout",["",[["impls",[["core",[["sync",[],["atomic.rs","mod.rs"]]],["array.rs","cell.rs","cmp.rs","convert.rs","ffi.rs","fn.rs","marker.rs","mem.rs","mod.rs","num.rs","ops.rs","option.rs","pin.rs","primitive.rs","ptr.rs","ref.rs","result.rs","tuple.rs"]]],["mod.rs"]]],["discriminant.rs","inhabited.rs","lib.rs","ser.rs","typeset.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[332]}
+//{"start":36,"fragment_lengths":[350]}
